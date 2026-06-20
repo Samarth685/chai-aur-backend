d7bf08e25e12770ecd08cd2d0d6e71fb3a572169
Samarth685 <samarth682005@gmail.com>
2026-06-20 10:57:41 +0530

    feat: implement user account management features including password change, account updates, and avatar/cover image uploads

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,6 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {registerUser,loginUser,logoutUser,refreshAccessToken,getCurrentUser,updateAccountDetails,changeCurrentPassword,updateUserAvatar,updateUserCoverImage} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
-import {loginUser} from "../controllers/user.controller.js";
-import {logoutUser} from "../controllers/user.controller.js";
-import {refreshAccessToken} from "../controllers/user.controller.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 const router = Router();
